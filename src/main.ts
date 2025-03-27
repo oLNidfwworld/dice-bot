178851f3c23fa93db117d2908102c4648b9fc925
oLNidfwworld <kuvetov@bk.ru>
2025-03-27 17:05:50 +0300

    do

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,17 @@ export default async function () {
 
   const bot = new Bot(process.env.API_BOT_TOKEN);
   bot.command("dice", (ctx) => {
-    ctx.replyWithPhoto(new InputFile(`./assets/img/dices/d${rand()}.png`));
+    let options = {};
+    if ( ctx.message?.message_id ) {
+        options = {
+            reply_parameters: {
+                message_id: ctx.message.message_id
+            }
+        }
+    }
+    ctx.replyWithPhoto(new InputFile(`./assets/img/dices/d${rand()}.png`), {
+        ...options
+    });
   });
 
   bot.start();
